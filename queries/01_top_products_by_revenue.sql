8c1706c6dca273a65e56a9f889846cb512d4e044
OlufunbiWill <Olufunbiwilliams@gmail.com>
2026-02-22 03:21:16 -0600

    Add: Query for top 5 products by revenue

---
+++ queries/01_top_products_by_revenue.sql
@@ -0,0 +1,14 @@
+-- Q1: What are our top 5 best-selling products by revenue?
+
+Select * from products;
+select * from order_items;
+
+select
+ p.product_name,
+ sum( i.quantity * i.item_price) as total_revenue
+from products as p
+join order_items as i
+on p.product_id = i.product_id
+group by p.product_name, p.product_id
+order by total_revenue desc
+limit 5;
